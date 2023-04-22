4f5358789a0bdab2182bbeb18a781e67bca85336
Sebastian <smcasanova17@hotmail.com>
2023-04-22 11:49:37 -0400

    first commit with some starter code

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from "react-router-dom"
+
+import { Navbar } from './components'
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+
+        </div>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
